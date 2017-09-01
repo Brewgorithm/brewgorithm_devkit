52e024b217c43fa015a91dd8a0d7c8ba1f1e0493
Eric Zhao <eric@whimmly.com>
2017-09-01 12:35:53 -0700

    Hotfix for initialization error

--- brewgorithm/__init__.py
+++ brewgorithm/__init__.py
@@ -1,2 +1,2 @@
 from . import language
-from .neural import beer2vec, beer_emb, filter_beers, keyword_parser, smalltalk, word_weighter
+from .neural import beer2vec, beer_emb, smalltalk, word_weighter
